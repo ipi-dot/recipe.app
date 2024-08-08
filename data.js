930180fa1d9fa578f3b41fe44deb083e0edd052e
ipi-dot <i.piso1998@gmail.com>
2024-08-08 11:12:44 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+export const data = {
+  hits: [
+    {
+      recipe: {
+        label: "Paleo Chocolate Covered Caramels",
+        image: "https://example.com/image.jpg",
+        dietLabels: ["Paleo"],
+        healthLabels: ["Vegetarian", "Gluten-Free"],
+        cautions: ["Contains sugar (Cookie)"],
+        mealType: ["Snack"],
+        dishType: ["Sweets"],
+        totalTime: 30,
+        ingredients: [
+          "1 cup sugar",
+          "2 tbsp butter",
+          "1/2
