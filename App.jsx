1637398dcfc090cf3c8da218f49443b37572ce65
ipi-dot <i.piso1998@gmail.com>
2024-08-08 17:11:22 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
-import RecipeListPage from './RecipeListPage';
-import RecipePage from './RecipePage';
-import { data } from './utils/data'; // Beispiel-Daten importieren
+import React, { useState } from "react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import RecipeListPage from "./RecipeListPage";
+import RecipePage from "./RecipePage";
+import { data } from "./data";
 
 function App() {
   const [selectedRecipe, setSelectedRecipe] = useState(null);
 
+  const handleRecipeSelect = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleBack = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
-    <div>
-      {selectedRecipe ? (
-        <RecipePage recipe={selectedRecipe} onBack={() => setSelectedRecipe(null)} />
-      ) : (
-        <RecipeListPage recipes={data.hits} onSelect={setSelectedRecipe} />
-      )}
-    </div>
+    <ChakraProvider>
+      <Box p={5}>
+        {selectedRecipe ? (
+          <RecipePage recipe={selectedRecipe} onBack={handleBack} />
+        ) : (
+          <RecipeListPage recipes={data.hits} onSelect={handleRecipeSelect} />
+        )}
+      </Box>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
